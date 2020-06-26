29674003b34a1785e4be0ba679810cb4ba522c00
mvtcode <macvantan@gmail.com>
2020-06-26 21:32:05 +0700

    Update esp8266WebserverSd.ino

--- esp8266WebserverSd.ino
+++ esp8266WebserverSd.ino
@@ -4,6 +4,14 @@
 #include <SPI.h>
 #include <SD.h>
 
+/*
+Pin connect: SD -> arduino nano
+CS -> D8
+SCK -> D5
+MOSI -> D7
+MISO -> D6
+*/
+
 IPAddress local_IP(192,168,4,1);
 IPAddress gateway(192,168,4,1);
 IPAddress subnet(255,255,255,0);
